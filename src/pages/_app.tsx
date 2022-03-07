3c5c3f100931a77398f252953268976b50954d67
Hajime Nakagawa <navyblue0914@icloud.com>
2022-03-07 15:46:11 +0900

    add next theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import "../styles/globals.css"
+import { ThemeProvider } from "next-themes"
+
 import type { AppProps } from "next/app"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
